d824a2d7e4d8269b20ff97a1ea7ad0d6e188cbbc
Riley <rsbeach97@gmail.com>
2021-04-22 16:02:19 -0700

    added basic nav-bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,12 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-
+      <nav class="nav">
+        <a class="nav-link" href="#">Riley Beach</a>
+        <a class="nav-link" href="#">About Me</a>
+        <a class="nav-link" href="#">Projects</a>
+        <a class="nav-link" href="#">Contact Me</a>
+      </nav>
     </div>
   )
 }
