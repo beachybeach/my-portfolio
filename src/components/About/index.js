ef26e1274f668a31c5a4fe3fb57dceb74bfaad28
Riley <rsbeach97@gmail.com>
2021-04-22 16:26:33 -0700

    super simple about me section to start with

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+
+  return (
+    <div>
+      <div class="jumbotron">
+        <h1 class="display-4">Hello, my name is Riley.</h1>
+        <p class="lead">I am a fullstack web developer from Arizona.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About;
