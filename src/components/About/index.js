ea2da454c434cf146ec9208e3149c78d579fdf6e
Riley <rsbeach97@gmail.com>
2021-04-23 18:32:18 -0700

    about me portion giving me trouble

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,10 +4,13 @@ const About = () => {
 
   return (
     <div>
-      <div class="jumbotron bg-transparent text-center">
-        <div className="me"></div>
-        <h1 class="display-4">Hello, my name is Riley.</h1>
-        <h2>I am a fullstack web developer from Arizona.</h2>
+      <div class="card-group about">
+
+        <div className="card me"></div>
+        <div className="card-about">
+          <h1 class="display-4">Hello, my name is Riley.</h1>
+          <h2>I am a fullstack web developer from Arizona.</h2>
+        </div>
       </div>
     </div>
   )
