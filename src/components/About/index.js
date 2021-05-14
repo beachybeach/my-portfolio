75991459aa2e62c47b9b0c69ce8213fe213e279b
Riley <rsbeach97@gmail.com>
2021-05-13 21:05:58 -0700

    updating about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,30 +5,66 @@ const About = () => {
   return (
     <div>
       <section id="about" class="about">
-        <div className="container">
-          <div className="section-title">
+        <div class="container">
+          <div class="section-title">
             <h2>About</h2>
             <p>Hello, my name is Riley Beach and I am a full stack web developer from Phoenix, Arizona.</p>
           </div>
-          <div className="row">
-            <div className="col-lg-4">
+          <div class="row">
+            <div class="col-lg-4">
               <img src={me} class="img-fluid" alt="self-portrait"></img>
             </div>
-            <div className="col-lg-8 pt-4 pt-lg-0">
+            <div class="col-lg-8 pt-4 pt-lg-0 content">
               <h3>Full Stack Web Developer</h3>
               <p>I am a bootcamp certified full stack web developer with a love for React but a desire for constant and continuous learning.</p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-6">
-              <ul>
-                <li>
-                  <i className="bi bi-chevron-right">
-                    <strong>Birthday:</strong>
-                    <span>1 August 1995</span>
-                  </i>
-                </li>
-              </ul>
+
+
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul>
+                    <li>
+                      <i class="fas fa-chevron-right"></i>
+                      <strong>Birthday:   </strong>
+                      <span>1 August 1995</span>
+
+                    </li>
+                    <li>
+                      <i class="fas fa-chevron-right"></i>
+                      <strong>Website:   </strong>
+                      <span>beachybeach.github.io/my-portfolio</span>
+
+                    </li>
+                    <li>
+                      <i class="fas fa-chevron-right"></i>
+                      <strong>City:   </strong>
+                      <span>Phoenix, Arizona</span>
+
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul>
+                    <li>
+                      <i class="fas fa-chevron-right"></i>
+                      <strong>Age:   </strong>
+                      <span>23</span>
+
+                    </li>
+                    <li>
+                      <i class="fas fa-chevron-right"></i>
+                      <strong>Degree:   </strong>
+                      <span>Bachelors in Marketing</span>
+
+                    </li>
+                    <li>
+                      <i class="fas fa-chevron-right"></i>
+                      <strong>Email:   </strong>
+                      <span>rsbeach97@gmail.com</span>
+
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
