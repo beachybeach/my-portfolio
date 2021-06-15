484a68fd94efdaf82764c47a5720dc03fa9ebe67
Riley <rsbeach97@gmail.com>
2021-06-14 20:14:53 -0700

    resized and added border to contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,93 +3,93 @@ import React from "react";
 const Contact = () => {
   return (
     <div class="mb-4" id="contact">
+      <div class="form">
+        <h2 class="h1-responsive font-weight-bold text-center my-4">Contact Me</h2>
 
-      <h2 class="h1-responsive font-weight-bold text-center my-4">Contact Me</h2>
+        <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact me.</p>
 
-      <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact me.</p>
-
-      <div class="row">
-
-
-        <div class="col-md-9 mb-md-0 mb-5">
-          <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+        <div class="row">
 
 
-            <div class="row">
+          <div class="col-md-9 mb-md-0 mb-5">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
 
 
-              <div class="col-md-6">
-                <div class="md-form mb-0">
-                  <input type="text" id="name" name="name" class="form-control" />
-                  <label for="name" class="">Your name</label>
+              <div class="row">
+
+
+                <div class="col-md-6">
+                  <div class="md-form mb-0">
+                    <input type="text" id="name" name="name" class="form-control" />
+                    <label for="name" class="">Your name</label>
+                  </div>
+                </div>
+
+
+
+                <div class="col-md-6">
+                  <div class="md-form mb-0">
+                    <input type="text" id="email" name="email" class="form-control" />
+                    <label for="email" class="">Your email</label>
+                  </div>
+                </div>
+
+
+              </div>
+
+
+
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="md-form mb-0">
+                    <input type="text" id="subject" name="subject" class="form-control" />
+                    <label for="subject" class="">Subject</label>
+                  </div>
                 </div>
               </div>
 
 
 
-              <div class="col-md-6">
-                <div class="md-form mb-0">
-                  <input type="text" id="email" name="email" class="form-control" />
-                  <label for="email" class="">Your email</label>
+              <div class="row">
+
+
+                <div class="col-md-12">
+
+                  <div class="md-form">
+                    <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                    <label for="message">Your message</label>
+                  </div>
+
                 </div>
               </div>
 
 
+            </form>
+
+            <div class="text-center text-md-left">
+              <a href="moviepoopshoot.com" class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
             </div>
-
-
-
-            <div class="row">
-              <div class="col-md-12">
-                <div class="md-form mb-0">
-                  <input type="text" id="subject" name="subject" class="form-control" />
-                  <label for="subject" class="">Subject</label>
-                </div>
-              </div>
-            </div>
-
-
-
-            <div class="row">
-
-
-              <div class="col-md-12">
-
-                <div class="md-form">
-                  <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                  <label for="message">Your message</label>
-                </div>
-
-              </div>
-            </div>
-
-
-          </form>
-
-          <div class="text-center text-md-left">
-            <a href="moviepoopshoot.com" class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+            <div class="status"></div>
           </div>
-          <div class="status"></div>
+
+
+
+          <div class="col-md-3 text-center">
+            <ul class="list-unstyled mb-0">
+              <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                <p>Phoenix, AZ USA</p>
+              </li>
+
+              <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                <p>+ 01 234 567 89</p>
+              </li>
+
+              <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                <p>rsbeach97@gmail..com</p>
+              </li>
+            </ul>
+          </div>
         </div>
-
-
-
-        <div class="col-md-3 text-center">
-          <ul class="list-unstyled mb-0">
-            <li><i class="fas fa-map-marker-alt fa-2x"></i>
-              <p>Phoenix, AZ USA</p>
-            </li>
-
-            <li><i class="fas fa-phone mt-4 fa-2x"></i>
-              <p>+ 01 234 567 89</p>
-            </li>
-
-            <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-              <p>rsbeach97@gmail..com</p>
-            </li>
-          </ul>
-        </div>
-
 
       </div>
       <div id="find-me">
