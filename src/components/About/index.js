5f1554e96b12d27154f0886a759f1382f51f11bb
Riley <rsbeach97@gmail.com>
2021-06-16 20:03:03 -0700

    re-organized about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,72 +4,26 @@ import me from '../../assets/images/profile.jpeg';
 const About = () => {
   return (
     <div>
-      <section id="about" class="about">
-        <div class="container">
-          <div class="section-title">
-            <h2>About</h2>
-            <p>Hello, my name is Riley Beach and I am a full stack web developer from Phoenix, Arizona.</p>
-          </div>
-          <div class="row">
-            <div class="col-lg-4">
-              <img src={me} class="img-fluid" alt="self-portrait"></img>
+      <div class="container">
+        <div id="about" class="container about">
+          <div class="row row--35 align-items-center">
+            <div class="col-lg-5">
+              <div class="thumbnail">
+                <img class="w-100" src={me} alt="me"></img>
+              </div>
             </div>
-            <div class="col-lg-8 pt-4 pt-lg-0 content">
-              <h3>Full Stack Web Developer</h3>
-              <p>I am a bootcamp certified full stack web developer with a love for React but a desire for constant and continuous learning.</p>
-
-
-              <div class="row">
-                <div class="col-lg-6">
-                  <ul>
-                    <li>
-                      <i class="fas fa-chevron-right"></i>
-                      <strong>Birthday:   </strong>
-                      <span>1 August 1995</span>
-
-                    </li>
-                    <li>
-                      <i class="fas fa-chevron-right"></i>
-                      <strong>Website:   </strong>
-                      <span>beachybeach.github.io/my-portfolio</span>
-
-                    </li>
-                    <li>
-                      <i class="fas fa-chevron-right"></i>
-                      <strong>City:   </strong>
-                      <span>  Phoenix, Arizona</span>
-
-                    </li>
-                  </ul>
-                </div>
-                <div class="col-lg-6">
-                  <ul>
-                    <li>
-                      <i class="fas fa-chevron-right"></i>
-                      <strong>Age:   </strong>
-                      <span>23</span>
-
-                    </li>
-                    <li>
-                      <i class="fas fa-chevron-right"></i>
-                      <strong>Degree:   </strong>
-                      <span>Bachelors in Marketing</span>
-
-                    </li>
-                    <li>
-                      <i class="fas fa-chevron-right"></i>
-                      <strong>Email:   </strong>
-                      <span>rsbeach97@gmail.com</span>
-
-                    </li>
-                  </ul>
+            <div class="col-lg-7">
+              <div class="about-inner inner">
+                <div class="section-title">
+                  <h2 class="title">About Me</h2>
+                  <p>I am a bootcamp certified full stack web developer with a love for React but a desire for constant and continuous learning.</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </div >
+      </div>
+    </div>
   )
 }
 
